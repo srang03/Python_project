e1be86a9967baaaf0a996e61d6895a349050abd5
srang03 <srang9503@gmail.com>
2020-10-04 16:52:03 +0900

    master

--- corona_data_analysis.py
+++ corona_data_analysis.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-corona_data = pd.read_excel('./data_file/corona_data_.xlsx', index_col=0)
+corona_data = pd.read_excel('./data_file/corona_data_.xlsx', index_col="날짜")
 print(corona_data.columns)
+
+print(corona_data['격리해제'])
+print(corona_data['확진자'])
 
 print(corona_data)
