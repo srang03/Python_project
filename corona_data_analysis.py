bfcc789a8bd4f083119a8d2904f8f2b73544dc37
srang03 <srang9503@gmail.com>
2020-10-04 17:28:39 +0900

    master

--- corona_data_analysis.py
+++ corona_data_analysis.py
@@ -3,7 +3,11 @@ import pandas as pd
 corona_data = pd.read_excel('./data_file/corona_data_.xlsx', index_col="날짜")
 print(corona_data.columns)
 
-print(corona_data['격리해제'])
-print(corona_data['확진자'])
+# 2020-03-03 코로나 누적검사 수
+print(corona_data.loc['2020-03-03','누적검사'])
+
+# 2020-03-03 코로나 사망자 수
+print('사망자는 {}명 입니다.'.format(corona_data.loc['2020-03-03','사망자']))
+
 
 print(corona_data)
